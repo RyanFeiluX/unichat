49622165079324fcbce8dfb1ceb85fbf5daa82d9
RyanX <looe_xiao@hotmail.com>
2025-03-21 15:40:22 +0800

    Comment some codes to temporarily disable webpage refresh-disappear fix.

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -42,28 +42,28 @@ function keyDown(e) {
   }
 }
 
-window.onbeforeunload = function(){
-    var html = document.querySelector("html");
-    sessionStorage.setItem("pre",html.innerHTML);
-}
+//window.onbeforeunload = function(){
+//    var html = document.querySelector("html");
+//    sessionStorage.setItem("pre",html.innerHTML);
+//}
 
-window.addEventListener("load",function(){
-    var before = sessionStorage.getItem("loadtime");
-    if(!before){
-        sessionStorage.setItem("loadtime",1);
-    }else{
-        sessionStorage.setItem("loadtime",parseInt(before)+1);
-    }//sessionStorage采用的是key-value形式，第二个参数为字符串类型，如果不对before值作整型处理，javascript会认为是字符串的拼接，这样就无法起到数字递增的效果
-    var storetime = sessionStorage.getItem("loadtime");
-    //console.log(storetime);
-    if(storetime>1){
-       var preHtml = sessionStorage.getItem("pre");
-       var html = document.querySelector("html");
-       html.innerHTML = preHtml;
-
-     }else if(storetime==1){
-        sessionStorage.removeItem("pre");
-     }
-
-    //后续控制交互行为或者处理业务的js代码
-})
+//window.addEventListener("load",function(){
+//    var before = sessionStorage.getItem("loadtime");
+//    if(!before){
+//        sessionStorage.setItem("loadtime",1);
+//    }else{
+//        sessionStorage.setItem("loadtime",parseInt(before)+1);
+//    }//sessionStorage采用的是key-value形式，第二个参数为字符串类型，如果不对before值作整型处理，javascript会认为是字符串的拼接，这样就无法起到数字递增的效果
+//    var storetime = sessionStorage.getItem("loadtime");
+//    //console.log(storetime);
+//    if(storetime>1){
+//       var preHtml = sessionStorage.getItem("pre");
+//       var html = document.querySelector("html");
+//       html.innerHTML = preHtml;
+//
+//     }else if(storetime==1){
+//        sessionStorage.removeItem("pre");
+//     }
+//
+//    //后续控制交互行为或者处理业务的js代码
+//})
