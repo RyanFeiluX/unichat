b56ca417757f5d69594b4a6f851e31719f590978
RyanX <looe_xiao@hotmail.com>
2025-04-03 12:28:44 +0800

    Support log file

---
+++ backend/logging_config.py
@@ -0,0 +1,29 @@
+import logging
+
+def setup_logging(logfile_path = None):
+    # Create a logger
+    logger = logging.getLogger('shared_logger')
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    if logfile_path:
+        # Create a file handler
+        file_handler = logging.FileHandler(logfile_path)
+        file_handler.setLevel(logging.INFO)
+
+        # Create a formatter and add it to the handler
+        file_handler.setFormatter(formatter)
+
+        # Add the handler to the logger
+        logger.addHandler(file_handler)
+
+    # Create a stream handler to print logs to the console
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(formatter)
+
+    # Add the stream handler to the logger
+    logger.addHandler(stream_handler)
+
+    return logger
