2f29423f913df6638f0cd0a12f02a017f606a1c7
RyanX <looe_xiao@hotmail.com>
2025-04-17 10:07:33 +0800

    Display downloaded ollama models in Ollama Setting window.

--- backend/ollama_setting.py
+++ backend/ollama_setting.py
@@ -1,9 +1,12 @@
 import os
 import shutil
+from typing import Tuple
 from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QFileDialog,
-                             QMessageBox, QProgressDialog, QLineEdit, QSpacerItem, QSizePolicy)
+                             QMessageBox, QProgressDialog, QLineEdit, QSpacerItem, QSizePolicy, QTableWidget,
+                             QTableWidgetItem)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
+import ollama
 
 
 class OllamaSetting:
@@ -54,6 +57,10 @@ class OllamaSetting:
         if directory:
             self.save_ollama_settings(directory, window, line_edit)
 
+    def get_ollama_models(self):
+        modlist = ollama.list()
+        return modlist.models
+
     # Function to open the Ollama settings window
     def open_ollama_settings(self):
         settings_window = QWidget()
@@ -72,13 +79,13 @@ class OllamaSetting:
 
         main_layout = QVBoxLayout()
 
-        # Label for Ollama model location
-        label = QLabel("Models Location:")
-        main_layout.addWidget(label)
-
         # Create a horizontal layout for the textbox and button
         h_layout = QHBoxLayout()
         h_layout.setSpacing(5)
+
+        # Label for Ollama model location
+        label = QLabel("Models Location:")
+        h_layout.addWidget(label, alignment=Qt.AlignLeft)
 
         # Readonly textbox
         line_edit = QLineEdit()
@@ -86,7 +93,7 @@ class OllamaSetting:
         current_location = os.getenv('OLLAMA_MODELS', '')
         line_edit.setText(current_location)
         line_edit.setStyleSheet("padding-top: 5px; padding-bottom: 5px; padding-left: 2px;")
-        h_layout.addWidget(line_edit, stretch=10, alignment=Qt.AlignLeft)
+        h_layout.addWidget(line_edit, stretch=10)
 
         # Directory picker button
         pick_button = QPushButton("...")
@@ -97,9 +104,48 @@ class OllamaSetting:
         # Add the horizontal layout to the main vertical layout
         main_layout.addLayout(h_layout)
 
-        # Add a vertical spacer to increase the space
-        spacer = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
-        main_layout.addItem(spacer)
+        # # Add a vertical spacer to increase the space
+        # spacer = QSpacerItem(5, 5, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        # main_layout.addItem(spacer)
+
+        label = QLabel("Downloaded Ollama Models:")
+        main_layout.addWidget(label, alignment=Qt.AlignLeft)
+
+        # Table to list all models
+        table = QTableWidget()
+        titles = ["Name", "Families", "Para#", "Size"]  #, "Quantization", "Format"
+        table.setColumnCount(len(titles))
+        table.setHorizontalHeaderLabels(titles)
+        models = self.get_ollama_models()
+        table.setRowCount(len(models))
+        table_width = table.size().width()
+        table.setColumnWidth(0, int(table_width * 0.5))
+        table.setColumnWidth(1, int(table_width * 0.2))
+        table.setColumnWidth(2, int(table_width * 0.16))
+        table.setColumnWidth(3, int(table_width * 0.16))
+        # Set the background color for the table header row
+        header = table.horizontalHeader()
+        header.setStyleSheet("QHeaderView::section { background-color: lightblue; }")
+
+        for i, model in enumerate(models):
+            model_name_item = QTableWidgetItem(model.model)
+            model_family_item = QTableWidgetItem(','.join(model.details.families) if model.details.families else '')
+            model_parasize_item = QTableWidgetItem(model.details.parameter_size)
+            try:
+                # Convert bytes to megabytes
+                n,u = self.bytes_to_gb(model.size)
+                model_bytesize_str = "{:.2f} {}".format(n, u)
+                model_bytesize_item = QTableWidgetItem(model_bytesize_str)
+            except AttributeError:
+                self.logger.error(f"Model {model.model} does not have a valid size attribute.")
+                model_bytesize_item = QTableWidgetItem("N/A")
+            # model_quant_item = QTableWidgetItem(model.details.quantization_level)
+            # model_format_item = QTableWidgetItem(model.details.format)
+            table.setItem(i, 0, model_name_item)
+            table.setItem(i, 1, model_family_item)
+            table.setItem(i, 2, model_parasize_item)
+            table.setItem(i, 3, model_bytesize_item)
+        main_layout.addWidget(table)
 
         # Close button in the bottom-right corner
         close_button = QPushButton("Close")
@@ -107,5 +153,23 @@ class OllamaSetting:
         main_layout.addWidget(close_button, alignment=Qt.AlignRight)
 
         settings_window.setLayout(main_layout)
-        settings_window.setFixedSize(600, 300)
+        settings_window.setFixedSize(800, 600)
         settings_window.show()
+
+    def bytes_to_gb(self, bytes_size)->Tuple[float, str]:
+        ret = (0, 'B')
+        try:
+            # Convert bytes to megabytes
+            granularity = ['B','K','M','G']
+            result = float(bytes_size)
+            r = (bytes_size, granularity[0])
+            for u in granularity[1:]:
+                if result > 1.0:
+                    result /= 1024
+                    r = (result, u)
+                else:
+                    break
+            ret = r
+        except ValueError:
+            self.logger.error(f"Invalid byte size value: {bytes_size}")
+        return ret
